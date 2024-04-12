0dbcfbfbf4090355f0197deeba77ec5bd5126221
BrandonLozada <brandon_lozada@outlook.com>
2024-04-12 00:22:29 -0600

    Implementar funcionalidad de "Confirmar reservación"

--- src/views/appointments/AppointmentView.vue
+++ src/views/appointments/AppointmentView.vue
@@ -1,10 +1,24 @@
 <script setup>
+    import { ref } from 'vue'
     import VueTailwindDatepicker from 'vue-tailwind-datepicker'
     import SelectedService from '@/components/SelectedService.vue'
     import { formatCurrency } from '@/helpers';
     import { useAppointmentsStore } from '@/stores/appointments'
 
     const appointments = useAppointmentsStore()
+
+    const formatter = ref({
+        date: 'DD/MM/YYYY',
+        month: 'MMM'
+    })
+
+    const disableDate = (date) => {
+        const today = new Date()
+        // true : cuando la fecha es menor a la de hoy
+        // true : cuando el mes de la fecha es mayor al mes actual más otro mes
+        // true : cuando los días domingo y sábado (0 y 6) de la semana sean los de las fechas en el callback
+        return date < today || date.getMonth() > today.getMonth() + 1 || [0,6].includes(date.getDay())
+    }
 </script>
 
 <template>
@@ -24,6 +38,42 @@
 
         <p class="text-right text-white text-2xl">Total a pagar:
             <span class="font-black">{{ formatCurrency(appointments.totalAmount) }}</span></p>
+    </div>
+
+    <div class="space-y-8" v-if="!appointments.noServicesSelected">
+        <h3 class="text-3xl font-extrabold text-white">Fecha y hora</h3>
+
+        <div class="lg:flex gap-5 items-start">
+            <div class="w-full lg:w-96 bg-white flex justify-center rounded-lg">
+                <vue-tailwind-datepicker
+                    :disable-date="disableDate"
+                    i18n="es-mx"
+                    as-single
+                    no-input
+                    :formatter="formatter"
+                    v-model="appointments.date"
+                />
+            </div>
+            <div class="flex-1 grid grid-cols-1 xl:grid-cols-2 gap-5 mt-10 lg:mt-0">
+                <button
+                    v-for="hour in appointments.hours"
+                    class="block text-blue-500 rounded-lg text-xl font-black p-3"
+                    :class="appointments.time === hour ? 'bg-blue-500 text-white' : 'bg-white'"
+                    @click="appointments.time = hour"
+                >
+                    {{ hour }}
+                </button>
+            </div>
+        </div>
+
+        <div v-if="appointments.isValidReservation" class="flex justify-end">
+            <button
+                class="w-full md:w-auto bg-blue-500 p-3 rounded-lg uppercase font-black text-white"
+                @click="appointments.createAppointment"
+            >
+                Confirmar reservación
+            </button>
+        </div>
     </div>
 
 </template>
