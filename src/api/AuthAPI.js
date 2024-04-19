aebefff6ae125a00719cc6b79896222d18b67cf3
Brandon Lozada <brandon_lozada@outlook.com>
2024-04-19 15:05:21 -0600

    Implementar función de inicio de sesión

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -6,5 +6,8 @@ export default {
     },
     verifyAccount(token) {
         return api.get(`auth/verify/${token}`)
+    },
+    signIn(data) {
+        return api.post('auth/sign-in', data)
     }
 }
--- src/views/auth/SignInView.vue
+++ src/views/auth/SignInView.vue
@@ -1,10 +1,67 @@
 <script setup>
+    import { inject } from 'vue'
+    import AuthAPI from '@/api/AuthAPI'
+
+    const toast = inject('toast')
+
+    const handleSubmit = async (formData) => {
+        try {
+            const { data } = await AuthAPI.signIn(formData)
+            toast.open({
+                    message: data.message,
+                    type: 'success'
+                })
+            } catch (error) {
+                toast.open({
+                    message: error.response.data.message,
+                    type: 'error'
+                })
+        }
+    }
 
 </script>
 
 <template>
-    <div>
-        <h1>Desde SignInView</h1>
-    </div>
+    <h1 class="text-6xl font-extrabold text-white text-center mt-10">Inicia sesión</h1>
+    <p class="text-2xl text-white text-center my-5">Si tienes una cuenta, inicia sesión</p>
+
+    <FormKit
+        id="signInForm"
+        type="form"
+        :actions="false"
+        incomplete-message="No se pudo enviar, revisa las notificaciones"
+        @submit="handleSubmit"
+        autocomplete="off"
+    >
+
+        <FormKit
+            type="email"
+            label="Correo"
+            name="email"
+            placeholder="Correo electrónico"
+            validation="required|email"
+            :validation-messages="{
+                required: 'El correo electrónico es obligatorio',
+                email: 'El correo electrónico no es válido'
+            }"
+        />
+
+        <FormKit
+            type="password"
+            label="Contraseña"
+            name="password"
+            placeholder="Contraseña"
+            validation="required"
+            :validation-messages="{ required: 'La contraseña es obligatoria' }"
+        />
+
+        <FormKit
+            type="submit"
+        >
+            Iniciar sesión
+        </FormKit>
+    </FormKit>
 </template>
+
+
 
