dc26e614400caaf118b462cfa50cf99479ebb8ad
BrandonLozada <brandon_lozada@outlook.com>
2024-04-27 01:18:05 -0600

    DONE: Visualizar mis citas en progreso

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -1,5 +1,6 @@
 <script setup>
     import { displayDate } from '@/helpers/date.js'
+    import { formatCurrency } from '@/helpers'
     defineProps({
         appointment: {
             type: Object
@@ -9,10 +10,34 @@
 
 <template>
     <div class="bg-white p-5 space-y-3 rounded-lg">
-        <p class="text-gray-500 font-black">
+        <p class="text-gray-500 font-black text-center">
             Fecha: <span class="font-light">{{ displayDate(appointment.date) }}</span>
             Hora: <span class="font-light">{{ appointment.time }} horas</span>
         </p>
+
+        <p class="text-gray-700 text-lg font-black">Servicios solicitados en la cita</p>
+        <div v-for="service in appointment.services">
+            <p>{{ service.name }}</p>
+            <p class="text-2xl font-black text-blue-500">{{ formatCurrency(service.price) }}</p>
+        </div>
+
+        <p class="text-2xl font-black text-right">
+            Total a pagar: <span class="text-blue-600">{{ formatCurrency(appointment.totalAmount) }}</span>
+        </p>
+
+        <div class="flex gap-2 items-center">
+            <button
+                class="bg-slate-600 rounded-xl p-3 text-white text-sm uppercase	font-black flex-1 md:flex-none"
+            >
+                Editar cita
+            </button>
+
+            <button
+                class="bg-red-600 rounded-xl p-3 text-white text-sm uppercase	font-black flex-1 md:flex-none"
+            >
+                Cancelar cita
+            </button>
+        </div>
     </div>
 </template>
 
