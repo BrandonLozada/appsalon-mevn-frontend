6139950e2ceecb47af57a1245bcf68417c408b30
Brandon Lozada <brandon_lozada@outlook.com>
2024-04-27 00:25:53 -0600

    WIP - Visualizar mis citas en progreso

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,19 +2,30 @@ import { ref, onMounted, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useRouter } from 'vue-router'
 import AuthAPI from '@/api/AuthAPI'
+import AppointmentAPI from '@/api/AppointmentAPI'
 
 export const useUserStore = defineStore('user', () => {
     const router = useRouter()
     const user = ref({})
+    const userAppointments = ref([])
+    const loadingAppointments = ref(true)
 
     onMounted(async () => {
         try {
             const { data } = await AuthAPI.auth()
             user.value = data
+            await getUserAppointments()
         } catch (error) {
             console.log(error)
+        } finally {
+            loadingAppointments.value = false
         }
     })
+
+    async function getUserAppointments() {
+        const { data } = await AppointmentAPI.getUserAppointments(user.value._id)
+        userAppointments.value = data
+    }
 
     function signOut(){
         localStorage.removeItem('AUTH_TOKEN')
@@ -33,9 +44,14 @@ export const useUserStore = defineStore('user', () => {
 
     const getUserName = computed(() => user.value?.name ? user.value?.name : '')
 
+    const noAppointments = computed(() => userAppointments.value.length === 0)
+
     return {
         user,
+        userAppointments,
+        loadingAppointments,
         signOut,
-        getUserName
+        getUserName,
+        noAppointments
     }
 })
